3993fea44f8fca470a24b2031d093538070c50e6
Amadeus Olofsson <amadeusolofsson@gmail.com>
2026-02-09 01:53:47 +0100

    Bytte tillbaks till parallellism_type

--- code/models/model_parallel.py
+++ code/models/model_parallel.py
@@ -161,13 +161,13 @@ def main():
                 total_images_processed = len(X) * 5
                 throughput = total_images_processed / training_time
                 total_batches = (len(X) / 64) * 5
-                avg_batch_latency = training_time / total_batches
+                avg_batch_latency = 1000 * (training_time / total_batches)
             else:
                 throughput = len(Xt) / test_time
-                avg_batch_latency = test_time / (len(Xt) / 64)
+                avg_batch_latency = 1000 * (test_time / (len(Xt) / 64))
 
             log = {
-                "model_type": "model_parallel",
+                "parallelism_type": "model_parallel",
                 "accuracy": float(final_acc),
                 "training_time": round(training_time, 2),
                 "test_time": round(test_time, 2),
